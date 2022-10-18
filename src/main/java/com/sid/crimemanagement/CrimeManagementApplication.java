f6814939505c82e0eefa714f6d94b0400344b541
siddheswarojha <siddheswarojha23@gmail.com>
2022-10-18 18:41:43 +0530

    initializing empty repo

---
+++ src/main/java/com/sid/crimemanagement/CrimeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sid.crimemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrimeManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sid/crimemanagement/CrimeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.crimemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
